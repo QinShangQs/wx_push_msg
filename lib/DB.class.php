9f120ba26b2ca164773489074dd7577898241ae9
qinshang <qinshang@cc.com>
2018-06-19 17:29:53 +0800

    fixed

--- lib/DB.class.php
+++ lib/DB.class.php
@@ -1,219 +1,233 @@
 <?php
-
 // 封装一个DB类，用来专门操作数据库，以后凡是对数据库的操作，都由DB类的对象来实现
 class DB {
-	private $host;
-	private $port;
-	private $user;
-	private $pass;
-	private $dbname;
-	private $charset;
-	private $prefix;
-	// 表前缀
-	private $link;
-	// 连接资源（连接数据库，一般会返回一个资源，所以需要定义一个link属性）
-	
-	// 构造方法(作用：为了初始化对象的属性)，会被自动调用
-	/*
-	 * @param1 array $arr，默认为空，里面是一个关联数组，里面有7个元素
-	 * array('host' => 'localhost','port' => '3306');
-	 */
-	public function __construct($arr = array()) {
-		// 初始化
-		$this->host = isset ( $arr ['host'] ) ? $arr ['host'] : 'localhost'; // 先判断是否有自己的host，如果有就用自己的host，否则就使用默认的localhost
-		$this->port = isset ( $arr ['port'] ) ? $arr ['port'] : '3306';
-		$this->user = isset ( $arr ['user'] ) ? $arr ['user'] : 'root';
-		$this->pass = isset ( $arr ['pass'] ) ? $arr ['pass'] : 'root';
-		$this->dbname = isset ( $arr ['dbname'] ) ? $arr ['dbname'] : 'mydatabase';
-		$this->charset = isset ( $arr ['charset'] ) ? $arr ['charset'] : 'utf8';
-		$this->prefix = isset ( $arr ['prefix'] ) ? $arr ['prefix'] : '';
-		
-		// 连接数据库（类是要操作数据库，因此要连接数据库）
-		$this->connect ();
-		
-		// 设置字符集
-		$this->setCharset ();
-		
-		// 选择数据库
-		$this->setDbname ();
-	}
-	
-	/*
-	 * 连接数据库
-	 */
-	private function connect() {
-		// mysql扩展连接
-		$this->link = mysql_connect ( $this->host . ':' . $this->port, $this->user, $this->pass );
-		
-		// 判断结果
-		if (! $this->link) {
-			// 结果出错了
-			// 暴力处理，如果是真实线上项目（生产环境）必须写入到日志文件
-			echo date('Ymd H:i:s').' 数据库连接错误：'."\n";
-			echo date('Ymd H:i:s').' 错误编号' . mysql_errno () . "\n";
-			echo date('Ymd H:i:s').' 错误内容' . mysql_error () . "\n";
-			exit ();
-		}
-	}
-	
-	public function reconnect(){
-		if(!mysql_ping($this->link)){
-			mysql_close($this->link);
-			$this->connect();
-			$this->setCharset ();
-			$this->setDbname ();
-			echo date('Ymd H:i:s').' 重建数据库连接'."\n";
-		}
-	}
-	
-	/*
-	 * 设置字符集
-	 */
-	private function setCharset() {
-		// 设置
-		$this->db_query ( "set names {$this->charset}" );
-	}
-	
-	/*
-	 * 选择数据库
-	 */
-	private function setDbname() {
-		$this->db_query ( "use {$this->dbname}" );
-	}
-	
-	/*
-	 * 增加数据
-	 * @param1 string $sql，要执行的插入语句
-	 * @return boolean，成功返回是自动增长的ID，失败返回FALSE
-	 */
-	public function db_insert($sql) {
-		// 发送数据
-		$this->db_query ( $sql );
-		
-		// 成功返回自增ID
-		return mysql_affected_rows () ? mysql_insert_id () : FALSE;
-	}
-	
-	/*
-	 * 删除数据
-	 * @param1 string $sql，要执行的删除语句
-	 * @return Boolean，成功返回受影响的行数，失败返回FALSE
-	 */
-	public function db_delete($sql) {
-		// 发送SQL
-		$this->db_query ( $sql );
-		
-		// 判断结果
-		return mysql_affected_rows () ? mysql_affected_rows () : FALSE;
-	}
-	
-	/*
-	 * 更新数据
-	 * @param1 string $sql，要执行的更新语句
-	 * @return Boolean，成功返回受影响的行数，失败返回FALSE
-	 */
-	public function db_update($sql) {
-		// 发送SQL
-		$this->db_query ( $sql );
-		
-		// 判断结果
-		return mysql_affected_rows () ? mysql_affected_rows () : FALSE;
-	}
-	
-	/*
-	 * 查询：查询一条记录
-	 * @param1 string $sql，要查询的SQL语句
-	 * @return mixed，成功返回一个数组，失败返回FALSE
-	 */
-	public function db_getRow($sql) {
-		// 发送SQL
-		$res = $this->db_query ( $sql );
-		
-		// 判断返回
-		return mysql_num_rows ( $res ) ? mysql_fetch_assoc ( $res ) : FALSE;
-	}
-	
-	/*
-	 * 查询：查询多条记录
-	 * @param1 string $sql，要查询的SQL语句
-	 * @return mixed，成功返回一个二维数组，失败返回FALSE
-	 */
-	public function db_getAll($sql) {
-		// 发送SQL
-		$res = $this->db_query ( $sql );
-		
-		// 判断返回
-		if (mysql_num_rows ( $res )) {
-			// 循环遍历
-			$list = array ();
-			
-			// 遍历
-			while ( $row = mysql_fetch_assoc ( $res ) ) {
-				$list [] = $row;
-			}
-			
-			// 返回
-			return $list;
-		}
-		
-		// 返回FALSE
-		return FALSE;
-	}
-	
-	/*
-	 * mysql_query错误处理
-	 * @param1 string $sql，需要执行的SQL语句
-	 * @return mixed，只要语句不出错，全部返回
-	 */
-	private function db_query($sql) {
-		// 发送SQL
-		$res = mysql_query ( $sql, $this->link );
-		
-		// 判断结果
-		if (! $res) {
-			// 结果出错了
-			// 暴力处理，如果是真实线上项目（生产环境）必须写入到日志文件
-			echo date('Ymd H:i:s').' 语句出现错误：'."\n";
-			echo $sql."\n";
-			echo date('Ymd H:i:s').' 错误编号' . mysql_errno () . "\n";
-			echo date('Ymd H:i:s').' 错误内容' . mysql_error () . "\n";
-			
-			if(mysql_errno() == 2006){//MySQL server has gone away
-				$this->reconnect();
-			}
-			//exit ();
-		}
-		// 没有错误
-		return $res;
-	}
-	// __sleep方法
-	public function __sleep() {
-		// 返回需要保存的属性的数组
-		return array (
-				'host',
-				'port',
-				'user',
-				'pass',
-				'dbname',
-				'charset',
-				'prefix' 
-		);
-	}
-	
-	// __wakeup方法
-	public function __wakeup() {
-		// 连接资源
-		$this->connect ();
-		// 设置字符集和选中数据库
-		$this->setCharset ();
-		$this->setDbname ();
-	}
-	
-	/*
-	 * 获取完整的表名
-	 */
-	protected function getTableName() {
-		// 完整表名：前缀+表名
-		return $this->prefix . $this->table;
-	}
+
+    private $host;
+    private $port;
+    private $user;
+    private $pass;
+    private $dbname;
+    private $charset;
+    private $prefix;
+    // 表前缀
+    private $link;
+    private $log_task = 'db';
+
+    // 连接资源（连接数据库，一般会返回一个资源，所以需要定义一个link属性）
+    // 构造方法(作用：为了初始化对象的属性)，会被自动调用
+    /*
+     * @param1 array $arr，默认为空，里面是一个关联数组，里面有7个元素
+     * array('host' => 'localhost','port' => '3306');
+     */
+    public function __construct($arr = array()) {
+        // 初始化
+        $this->host = isset($arr ['host']) ? $arr ['host'] : 'localhost'; // 先判断是否有自己的host，如果有就用自己的host，否则就使用默认的localhost
+        $this->port = isset($arr ['port']) ? $arr ['port'] : '3306';
+        $this->user = isset($arr ['user']) ? $arr ['user'] : 'root';
+        $this->pass = isset($arr ['pass']) ? $arr ['pass'] : 'root';
+        $this->dbname = isset($arr ['dbname']) ? $arr ['dbname'] : 'mydatabase';
+        $this->charset = isset($arr ['charset']) ? $arr ['charset'] : 'utf8';
+        $this->prefix = isset($arr ['prefix']) ? $arr ['prefix'] : '';
+
+        // 连接数据库（类是要操作数据库，因此要连接数据库）
+        $this->connect();
+
+        // 设置字符集
+        $this->setCharset();
+
+        // 选择数据库
+        $this->setDbname();
+    }
+
+    /*
+     * 连接数据库
+     */
+    private function connect() {
+        // mysql扩展连接
+        $this->link = mysql_connect($this->host . ':' . $this->port, $this->user, $this->pass);
+
+        // 判断结果
+        if (!$this->link) {
+            // 结果出错了
+            // 暴力处理，如果是真实线上项目（生产环境）必须写入到日志文件
+            Log::_write($this->log_task, "数据库连接错误：");
+            Log::_write($this->log_task, '错误编号' . mysql_errno());
+            Log::_write($this->log_task, '错误内容' . mysql_error());
+            exit();
+        }
+    }
+
+    public function reconnect() {
+        if (!mysql_ping($this->link)) {
+            mysql_close($this->link);
+            $this->connect();
+            $this->setCharset();
+            $this->setDbname();
+            Log::_write($this->log_task, "重建数据库连接");
+        }
+    }
+
+    /*
+     * 设置字符集
+     */
+
+    private function setCharset() {
+        // 设置
+        $this->db_query("set names {$this->charset}");
+    }
+
+    /*
+     * 选择数据库
+     */
+
+    private function setDbname() {
+        $this->db_query("use {$this->dbname}");
+    }
+
+    /*
+     * 增加数据
+     * @param1 string $sql，要执行的插入语句
+     * @return boolean，成功返回是自动增长的ID，失败返回FALSE
+     */
+
+    public function db_insert($sql) {
+        // 发送数据
+        $this->db_query($sql);
+
+        // 成功返回自增ID
+        return mysql_affected_rows() ? mysql_insert_id() : FALSE;
+    }
+
+    /*
+     * 删除数据
+     * @param1 string $sql，要执行的删除语句
+     * @return Boolean，成功返回受影响的行数，失败返回FALSE
+     */
+
+    public function db_delete($sql) {
+        // 发送SQL
+        $this->db_query($sql);
+
+        // 判断结果
+        return mysql_affected_rows() ? mysql_affected_rows() : FALSE;
+    }
+
+    /*
+     * 更新数据
+     * @param1 string $sql，要执行的更新语句
+     * @return Boolean，成功返回受影响的行数，失败返回FALSE
+     */
+
+    public function db_update($sql) {
+        // 发送SQL
+        $this->db_query($sql);
+
+        // 判断结果
+        return mysql_affected_rows() ? mysql_affected_rows() : FALSE;
+    }
+
+    /*
+     * 查询：查询一条记录
+     * @param1 string $sql，要查询的SQL语句
+     * @return mixed，成功返回一个数组，失败返回FALSE
+     */
+
+    public function db_getRow($sql) {
+        // 发送SQL
+        $res = $this->db_query($sql);
+
+        // 判断返回
+        return mysql_num_rows($res) ? mysql_fetch_assoc($res) : FALSE;
+    }
+
+    /*
+     * 查询：查询多条记录
+     * @param1 string $sql，要查询的SQL语句
+     * @return mixed，成功返回一个二维数组，失败返回FALSE
+     */
+
+    public function db_getAll($sql) {
+        // 发送SQL
+        $res = $this->db_query($sql);
+
+        // 判断返回
+        if (mysql_num_rows($res)) {
+            // 循环遍历
+            $list = array();
+
+            // 遍历
+            while ($row = mysql_fetch_assoc($res)) {
+                $list [] = $row;
+            }
+
+            // 返回
+            return $list;
+        }
+
+        // 返回FALSE
+        return FALSE;
+    }
+
+    /*
+     * mysql_query错误处理
+     * @param1 string $sql，需要执行的SQL语句
+     * @return mixed，只要语句不出错，全部返回
+     */
+
+    private function db_query($sql) {
+        // 发送SQL
+        $res = mysql_query($sql, $this->link);
+
+        // 判断结果
+        if (!$res) {
+            // 结果出错了
+            // 暴力处理，如果是真实线上项目（生产环境）必须写入到日志文件
+            Log::_write($this->log_task, "语句出现错误：");
+            Log::_write($this->log_task, $sql);
+            Log::_write($this->log_task, '错误编号' . mysql_errno());
+            Log::_write($this->log_task, '错误内容' . mysql_error());
+
+            if (mysql_errno() == 2006) {//MySQL server has gone away
+                $this->reconnect();
+            }
+            //exit ();
+        }
+        Log::_write($this->log_task, $sql);
+        Log::_write($this->log_task, 'sql result is ' . $res);
+        // 没有错误
+        return $res;
+    }
+
+    // __sleep方法
+    public function __sleep() {
+        // 返回需要保存的属性的数组
+        return array(
+            'host',
+            'port',
+            'user',
+            'pass',
+            'dbname',
+            'charset',
+            'prefix'
+        );
+    }
+
+    // __wakeup方法
+    public function __wakeup() {
+        // 连接资源
+        $this->connect();
+        // 设置字符集和选中数据库
+        $this->setCharset();
+        $this->setDbname();
+    }
+
+    /*
+     * 获取完整的表名
+     */
+
+    protected function getTableName() {
+        // 完整表名：前缀+表名
+        return $this->prefix . $this->table;
+    }
+
 }
--- src/BlcgPush.class.php
+++ src/BlcgPush.class.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once 'Base.class.php';
 /**
  * 办理成功通知，只用一次
@@ -6,95 +7,113 @@ include_once 'Base.class.php';
  * @date 20180110
  */
 class BlcgPush extends Base {
-	public function __construct(){
-		parent::__construct();
-	}
-	
-	public function run($page, $mode){
-		$task = "blcg_{$page}_{$mode}";
-		
-		$openids = $this->getOpenIds($mode);
-		$tmpMsg = $this->getTemplateMsg($mode);
-		$total_count = count($openids);
-		$success_count = 0;
-		
-		for($i = 0; $i < $total_count; $i ++){
-			$openId = $openids[$i];
-			$tmpMsg['touser'] = $openId;
-			$result = $this->_wxApi->send_template_mssage(json_encode($tmpMsg));
-			Log::_write($task, "{$openId}\t".json_encode($result) );
-			if($result['errcode'] == 0){
-				$success_count ++ ;
-			}
-			
-			Log::_write($task, "===============================================================================");
-			Log::_write($task, "总数：{$total_count}\t成功:{$success_count}\t失败:".($total_count-$success_count));
-			Log::_write($task, "===============================================================================");
-		}
-	}
-	
-	private function getOpenIds($mode){
-		if($mode == ENV_DEV || $mode == ENV_TEST){
-			return array(WX_T_OPID);
-		}
-		
-		$sql = "select openid from `user` where vip_flg = 2;";
-		$rows = $this->_db->db_getAll($sql);
-		$datas = array();
-		foreach ($rows as $k => $v){
-			$datas[] = $v['openid'];
-		}
-		
-		return $datas;
-	}
-	
-	private function getTemplateMsg($mode){
-		$templateId = null;
-		if($mode == ENV_DEV ){
-			$templateId = "4gUv9ztGaRfm3WXJ6XeC5_wRk_gbyelouweuP1GKfjM";
-		}else{
-			$templateId = "GG3R3N26F5bTtSAz0E8Wk1Q_KZPQAOaoW0LXpnAs5X0";
-		}
-		
-		$data = array(
-				'touser' => '',
-				'template_id' => $templateId,
-				'url' => "http://m.hrwq.com/vip/records?from=singlemessage",
-				'topcolor' => '',
-				'data' => array(
-						'first' => array(
-								'value' => ("亲爱的和会员，因服务器升级导致本周一的课程更新延迟，特送您30天会员补偿，目前会员补偿已到账！\n"),
-								'color' => ''
-						),
-						'keyword1' => array(
-								'value' => ("VIP和会员\n"),
-								'color' => ''
-						),
-						'keyword2' => array(
-								'value' => ("2018-1-10\n"),
-								'color' => ''
-						),
-						'remark' => array(
-								'value' => ("点此查看我的会员天数！"),
-								'color' => ''
-						)
-				)
-		);
-		
-		return ($data);
-	}
+
+    private $task = "";
+    public function __construct() {
+        parent::__construct();
+    }
+
+    const AWARD_DAY = 21;
+    const AWARD_SOURCE = 6;
+
+    public function run($page, $mode) {
+        $this->task = "blcg_{$page}_{$mode}";
+
+        $openids = $this->getOpenIds($mode);
+        $tmpMsg = $this->getTemplateMsg($mode);
+        $total_count = count($openids);
+        $success_count = 0;
+
+        for ($i = 0; $i < $total_count; $i ++) {
+            $openId = $openids[$i];
+            $tmpMsg['touser'] = $openId;
+            $result = $this->_wxApi->send_template_mssage(json_encode($tmpMsg));
+            Log::_write($this->task, "{$openId}\t" . json_encode($result));
+            if ($result['errcode'] == 0) {
+                $success_count ++;
+            }
+
+            Log::_write($this->task, "===============================================================================");
+            Log::_write($this->task, "总数：{$total_count}\t成功:{$success_count}\t失败:" . ($total_count - $success_count));
+            Log::_write($this->task, "===============================================================================");
+        }
+    }
+
+    private function getOpenIds($mode) {
+        $sql = "select openid, id, nickname,mobile,realname from `user` where vip_flg = 2";
+        if($mode != ENV_PRODUCT){
+            $sql .= " and openid in ('ot3XZtyEcBJWjpXJxxyqAcpBCdGY','obpqNs_GdrHPLOGJig50qNcFZRGk')";
+        }
+        $rows = $this->_db->db_getAll($sql);
+        $datas = array();
+        foreach ($rows as $k => $v) {
+            $datas[] = $v['openid'];
+
+            $isql = "insert into user_point_vip(user_id,point_value,source,created_at) "
+                    . " values('{$v['id']}'," . self::AWARD_DAY . "," . self::AWARD_SOURCE . ",'".date('Y-m-d H:i:s')."');";
+            $ires = $this->_db->db_insert($isql);
+            if($ires){
+                $usql = "update `user` set vip_left_day = date_add(vip_left_day, interval " . self::AWARD_DAY . " day) where id = {$v['id']};";
+                $this->_db->db_update($usql);
+            }else{
+                Log::_write($this->task,  "{$k} is 插入失败".$isql);
+                exit;
+            }
+            
+            Log::_write($this->task,  "{$k} is ". json_encode($v, JSON_UNESCAPED_UNICODE));
+        }
+
+        return $datas;
+    }
+
+    private function getTemplateMsg($mode) {
+        $templateId = null;
+        if ($mode == ENV_DEV) {
+            $templateId = "4gUv9ztGaRfm3WXJ6XeC5_wRk_gbyelouweuP1GKfjM";
+        } else {
+            $templateId = "GG3R3N26F5bTtSAz0E8Wk1Q_KZPQAOaoW0LXpnAs5X0";
+        }
+
+        $data = array(
+            'touser' => '',
+            'template_id' => $templateId,
+            'url' => "http://m.hrwq.com/vip/records?from=singlemessage",
+            'topcolor' => '',
+            'data' => array(
+                'first' => array(
+                    'value' => ("亲爱的和会员，因服务器升级导致本周一的课程更新延迟，特送您" . self::AWARD_DAY . "天会员补偿，目前会员补偿已到账！\n"),
+                    'color' => ''
+                ),
+                'keyword1' => array(
+                    'value' => ("VIP和会员\n"),
+                    'color' => ''
+                ),
+                'keyword2' => array(
+                    'value' => (date('Y-m-d') . "\n"),
+                    'color' => ''
+                ),
+                'remark' => array(
+                    'value' => ("点此查看我的会员天数！"),
+                    'color' => ''
+                )
+            )
+        );
+
+        return ($data);
+    }
+
 }
 
 //获取shell参数
 $page = $argv[1];
 $mode = $argv[2];
 
-if(empty($page) || !is_numeric($page)){
-	throw new Exception('请正确传入页码!');
+if (empty($page) || !is_numeric($page)) {
+    throw new Exception('请正确传入页码!');
 }
 // 模式验证
 _mode_validate($mode);
 
 //运行主函数
 $object = new BlcgPush();
-$object->run ($page, $mode);
+$object->run($page, $mode);
